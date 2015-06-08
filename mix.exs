af23a3c135fad1e60c6ba6ae31973f179de3de82
Steven Schobert <spschobert@gmail.com>
2015-06-08 02:17:10 -0500

    move erlang lib into project
    hex.pm cannot handle non-hex packages

--- mix.exs
+++ mix.exs
@@ -24,7 +24,6 @@ defmodule Weebo.Mixfile do
   end
 
   defp deps do
-    [{:ex_doc, "~> 0.7", only: :docs},
-     {:iso8601, github: "seansawyer/erlang_iso8601", tag: "1.1.1", compile: "#{Mix.Rebar.local_rebar_path} compile"}]
+    [{:ex_doc, "~> 0.7", only: :docs}]
   end
 end
